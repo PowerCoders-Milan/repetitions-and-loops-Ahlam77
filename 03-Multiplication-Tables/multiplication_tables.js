e0bd000854f712530370e46cba1db53655318ca8
ahlam alshaba <ama1977ln@outlook.com>
2021-03-01 12:33:28 +0100

    solved

--- 03-Multiplication-Tables/multiplication_tables.js
+++ 03-Multiplication-Tables/multiplication_tables.js
@@ -1,1 +1,14 @@
 // write your code below
+var multi = 9;
+for (var i = 0; i <= 10; i++) {
+    var result = multi * i;
+    console.log(multi + ' * ' + i + ' = ' + result);
+}
+
+
+for (var multi = 0; multi <= 10; multi++) {
+ for (var i = 0; i <= 10; i++) {
+   var result = multi * i;
+   console.log(multi + ' * ' + i + ' = ' + result);
+ }
+}
